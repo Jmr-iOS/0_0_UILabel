a86e3ba7ab8b6074d37a1ad3deba5483cae11b3d
justinmreina <justinmreina@gmail.com>
2017-11-06 21:19:46 -0800

    (C)    Swift 3 update

--- 0_0 - UILabel/ViewController.swift
+++ 0_0 - UILabel/ViewController.swift
@@ -32,17 +32,17 @@ class ViewController: UIViewController {
     }
     
     
-    func addLabel(view:UIView) {
+    func addLabel(_ view:UIView) {
         
         let myFirstLabel = UILabel();
         
         myFirstLabel.text = "I made a label on the screen #toogood4you";
         myFirstLabel.font = UIFont(name: "MarkerFelt-Thin", size: 45);
-        myFirstLabel.textColor = UIColor.redColor();
-        myFirstLabel.textAlignment = .Center;
+        myFirstLabel.textColor = UIColor.red;
+        myFirstLabel.textAlignment = .center;
         myFirstLabel.numberOfLines = 5;
         
-        myFirstLabel.frame = CGRectMake(15, 54, 300, 500);
+        myFirstLabel.frame = CGRect(x: 15, y: 54, width: 300, height: 500);
         
         myFirstLabel.translatesAutoresizingMaskIntoConstraints = false;
         
@@ -52,15 +52,15 @@ class ViewController: UIViewController {
     }
     
     
-    func addSecondLabel(view: UIView) {
+    func addSecondLabel(_ view: UIView) {
         
         
         let newLabel : UILabel = UILabel(frame: CGRect(x:245, y: 115, width: 55, height:  22));      //values selected emperically
         
         newLabel.font  =   UIFont(name: "HelveticaNeue", size: 13);
         newLabel.text  =   "4:30 PM";
-        newLabel.textColor     = UIColor.blackColor();
-        newLabel.textAlignment = NSTextAlignment.Left;
+        newLabel.textColor     = UIColor.black;
+        newLabel.textAlignment = NSTextAlignment.left;
         newLabel.backgroundColor    = UIColor(red: 255/255, green:  60/255, blue:  60/255, alpha: 1);
         newLabel.clipsToBounds      = true; //required in order for clipping to show!
         newLabel.layer.cornerRadius = 12;
